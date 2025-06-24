a915c1cbaeeadbfcccc879c69ccbe12bad870993
soralatul16 <atulsoralphysicism@gmail.com>
2025-06-24 16:40:13 +0530

    Organized files and added starter content
    git add .

---
+++ dp-mcq-database.js
@@ -0,0 +1,64 @@
+const questionsDB = [
+  {
+    topic: "Measurements",
+    q: "Which of the following contains one fundamental and one derived unit?",
+    a: "D. joule and coulomb",
+    options: [
+      "A. ampere and kilogram",
+      "B. ampere and coulomb",
+      "C. joule and newton",
+      "D. joule and coulomb"
+    ]
+  },
+  {
+    topic: "Measurements",
+    q: "The current, I, through a resistor is measured with a digital ammeter to be 0.10 A. The uncertainty in the calculated value of I² will be",
+    a: "B. 2 %",
+    options: ["A. 1 %", "B. 2 %", "C. 5 %", "D. 20 %"]
+  },
+  {
+    topic: "Kinematics",
+    q: "After 25 seconds Joseph has run 200 m. Which of the following is correct at 25 seconds?",
+    a: "C. 10 m/s, 8 m/s",
+    options: [
+      "A. 8 m/s, 8 m/s",
+      "B. 8 m/s, 10 m/s",
+      "C. 10 m/s, 8 m/s",
+      "D. 10 m/s, 10 m/s"
+    ]
+  },
+  {
+    topic: "Electricity",
+    q: "What is Ohm’s Law?",
+    a: "A. V = IR",
+    options: [
+      "A. V = IR",
+      "B. P = IV",
+      "C. F = ma",
+      "D. v = fλ"
+    ]
+  },
+  {
+    topic: "Energy",
+    q: "A power station produces 10 GW of electrical power. Power from the nuclear core is 25 GW. What is its efficiency?",
+    a: "C. 40 %",
+    options: ["A. 15 %", "B. 35 %", "C. 40 %", "D. 60 %"]
+  },
+  {
+    topic: "Forces & Motion",
+    q: "A skydiver of mass 80 kg falls vertically with a constant speed of 50 m/s. The upward force acting on the skydiver is approximately",
+    a: "D. 4000 N",
+    options: ["A. 0 N", "B. 80 N", "C. 800 N", "D. 4000 N"]
+  },
+  {
+    topic: "Circular Motion",
+    q: "A cyclist rides around a circular track at uniform speed. What is the net horizontal force?",
+    a: "B. zero, non-zero",
+    options: [
+      "A. zero, zero",
+      "B. zero, non zero",
+      "C. non zero, zero",
+      "D. non zero, non zero"
+    ]
+  }
+];
