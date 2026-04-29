d31975bee13c29eb8b93b359ef522333d428ee41
soralatul16 <atulsoralphysicism@gmail.com>
2026-04-29 11:49:51 +0530

    Update question-bank.js

--- quiz/question-bank.js
+++ quiz/question-bank.js
@@ -1,1 +1,48 @@
+const questionBank = [
 
+{
+topic:"waves",
+level:"easy",
+question:"What is wave speed?",
+options:[
+"Distance travelled per unit time",
+"Frequency of wave",
+"Energy of wave",
+"Amplitude"
+],
+answer:"Distance travelled per unit time"
+},
+
+{
+topic:"waves",
+level:"medium",
+question:"Unit of frequency?",
+options:["Hz","m","J","N"],
+answer:"Hz"
+},
+
+{
+topic:"waves",
+level:"hard",
+question:"Which wave is longitudinal?",
+options:["Light","Sound","Water","EM"],
+answer:"Sound"
+},
+
+{
+topic:"atomic",
+level:"easy",
+question:"Who proposed nuclear model?",
+options:["Dalton","Thomson","Rutherford","Bohr"],
+answer:"Rutherford"
+},
+
+{
+topic:"electromagnetism",
+level:"easy",
+question:"Unit of current?",
+options:["Volt","Ampere","Ohm","Watt"],
+answer:"Ampere"
+}
+
+];
