be68a5d65fa23a39abcd23d9288e7d9168224b09
soralatul16 <atulsoralphysicism@gmail.com>
2026-04-30 12:42:22 +0530

    Update chapters.js

--- js/chapters.js
+++ js/chapters.js
@@ -2,6 +2,32 @@ const chapters = {
 
   waves: [
     // ✅ PASTE ALL YOUR WAVES IMAGE NAMES HERE
+"2-waves-introduction.png",
+"3-transverse-vs-longitudinal-wave.png",
+"4-basic-properties-of-waves.png",
+"5-wave-equation.png",
+"6-reflection-of-waves.png",
+"6-refraction-of-waves.png",
+"7-diffraction-of-waves.png",
+"8-total-internal-reflection.png",
+"9-superposition-of-waves.png",
+"10-standing-waves.png",
+"11-sound-waves.png",
+"12-speed-of-sound.png",
+"13-musical-applications.png",
+"14-electromagnetic-waves.png",
+"15-radio-and-microwaves.png",
+"16-infrared-visible-ultraviolet.png",
+"17-x-ray-and-gamma-ray.png",
+"18-reflection-and-refraction-of-light.png",
+"19-light-and-communication.png",
+"20-waves-and-culture.png",
+"21-Design_investigation.png",
+"22-data-processing.png",
+"23-wave-technology.png",
+"24-unit-project-showcase.png",
+"25-explore-waves-at-home-lab.png",
+"26-waves-connect-the-world.png"
   ],
 
   atomic: [
