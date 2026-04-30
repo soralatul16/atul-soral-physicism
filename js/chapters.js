c5dd01d3469134c7a697bbfd6478ce776bf37156
soralatul16 <atulsoralphysicism@gmail.com>
2026-04-30 12:39:32 +0530

    Create chapters.js

---
+++ js/chapters.js
@@ -0,0 +1,47 @@
+const chapters = {
+
+  waves: [
+    "2-waves-introduction.png",
+    "3-transverse-vs-longitudinal-wave.png",
+    "4-basic-properties-of-waves.png",
+    "5-wave-equation.png",
+    "6-reflection-of-waves.png",
+    "6-refraction-of-waves.png",
+    "7-diffraction-of-waves.png",
+    "8-total-internal-reflection.png",
+    "9-superposition-of-waves.png",
+    "10-standing-waves.png",
+    "11-sound-waves.png",
+    "12-speed-of-sound.png",
+    "13-musical-applications.png",
+    "14-electromagnetic-waves.png"
+  ],
+
+  atomic: [
+    "1-atomic-models.png",
+    "2-dalton.png",
+    "3-thomson.png",
+    "4-rutherford.png",
+    "5-bohr.png"
+  ],
+
+  electromagnetism: [
+    "1-static-electricity.png",
+    "2-charging.png",
+    "3-electric-field.png",
+    "4-magnetic-field.png"
+  ],
+
+  heat: [
+    "1-heat-transfer.png",
+    "2-specific-heat.png",
+    "3-latent-heat.png"
+  ],
+
+  energy: [
+    "1-forms-of-energy.png",
+    "2-energy-conservation.png",
+    "3-power.png"
+  ]
+
+};
