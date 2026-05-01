89250afcfa19250eab901d67a68aa6efc2c2a666
soralatul16 <atulsoralphysicism@gmail.com>
2026-05-01 20:10:16 +0530

    Update assessment-data.js

--- quiz/assessment-data.js
+++ quiz/assessment-data.js
@@ -2,30 +2,34 @@ const assessments = [
 
 {
   topic: "electromagnetism",
-  title: "Maglev Train Case Study",
   criterion: "D",
+  title: "Maglev Train Case Study",
 
   image: "1-introduction-to-static-electricity",
 
   parts: [
 
     {
-      question: "Describe how the train is lifted using magnetic forces.",
+      command: "Describe",
+      question: "how the train is lifted using magnetic forces.",
       marks: 2
     },
 
     {
-      question: "Explain why reducing friction improves efficiency.",
+      command: "Explain",
+      question: "why reducing friction improves efficiency.",
       marks: 3
     },
 
     {
-      question: "Describe the trend shown in the graph.",
+      command: "Analyse",
+      question: "the trend shown in the graph.",
       marks: 3
     },
 
     {
-      question: "Evaluate the advantages and disadvantages of maglev trains.",
+      command: "Evaluate",
+      question: "the advantages and disadvantages of maglev trains.",
       marks: 6
     }
 
