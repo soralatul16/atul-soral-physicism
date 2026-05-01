cd358f104c0f2d52edb88d8c67685a58eff46e0c
soralatul16 <atulsoralphysicism@gmail.com>
2026-05-01 20:13:13 +0530

    Update assessment-engine.js

--- quiz/assessment-engine.js
+++ quiz/assessment-engine.js
@@ -1,20 +1,18 @@
 function loadAssessment(){
 
-  const selected = assessments[0];
-
-  let html = `<h2>${selected.title}</h2>`;
-  html += `<p><b>Criterion:</b> ${selected.criterion}</p><hr>`;
+  let html = `<h2>${assessment.title}</h2>`;
+  html += `<p><b>${assessment.criterion}</b></p><hr>`;
 
   html += `
-    <img src="../Images/${selected.image}.png" 
+    <img src="../Images/${assessment.image}.png" 
          style="max-width:100%; margin-bottom:20px;">
   `;
 
-  selected.parts.forEach((p,i)=>{
+  assessment.questions.forEach((q,i)=>{
     html += `
       <div class="question">
-        <b>Q${i+1} (${p.marks} marks)</b><br><br>
-        ${p.question}
+        <b>Q${i+1} (${q.marks} marks)</b><br><br>
+        ${q.text}
         <textarea rows="4"></textarea>
       </div>
     `;
