46f23b9b0d3689a9028b35a85cb2c79b29d0cb05
soralatul16 <atulsoralphysicism@gmail.com>
2026-05-04 10:29:04 +0530

    Update player.js

--- quiz/player.js
+++ quiz/player.js
@@ -1,6 +1,3 @@
-let papers = JSON.parse(localStorage.getItem("papers") || "[]");
-
-/* LOAD SIDEBAR */
 function loadUnits(){
 
 let units = [...new Set(papers.map(p=>p.unit))];
@@ -8,64 +5,52 @@ let units = [...new Set(papers.map(p=>p.unit))];
 let html="";
 
 units.forEach(u=>{
-html+=`<div onclick="loadTopics('${u}')">${u}</div>`;
+html+=`<div onclick="loadPapers('${u}')">${u}</div>`;
 });
 
 document.getElementById("sidebar").innerHTML=html;
 }
 
-/* TOPICS */
-function loadTopics(unit){
+function loadPapers(unit){
 
-let topics = papers.filter(p=>p.unit===unit);
+let filtered = papers.filter(p=>p.unit===unit);
 
 let html="";
 
-topics.forEach(p=>{
-html+=`<div onclick="openPaper('${p.id}')">${p.title}</div>`;
+filtered.forEach(p=>{
+html+=`<div onclick="openPaper(${p.id})">${p.title}</div>`;
 });
 
-document.getElementById("main").innerHTML=html;
+document.getElementById("sidebar").innerHTML=html;
 }
 
-/* OPEN PAPER */
 function openPaper(id){
 
 let p = papers.find(x=>x.id===id);
 
-let html=`<h2>${p.title}</h2><p>${p.scenario}</p>`;
+let html = `<div class="paper">
+<h2>${p.title}</h2>
+<p>${p.scenario}</p>`;
 
 /* CONTENT */
 p.content.forEach(c=>{
-if(c.type==="image"){
-html+=`<img src="${c.src}" width="300">`;
-}
-if(c.type==="video"){
-html+=`<iframe src="${c.src}" width="400"></iframe>`;
-}
+if(c.type==="image") html+=`<img src="${c.url}">`;
+if(c.type==="video") html+=`<iframe src="${c.url}"></iframe>`;
 });
 
 /* QUESTIONS */
 p.questions.forEach((q,i)=>{
-
-html+=`<div class="question-box">
-<b>Q${i+1}</b> ${q.question}`;
-
-if(q.type==="mcq"){
-q.options.forEach(opt=>{
-html+=`<br><input type="radio">${opt}`;
-});
-}
+html+=`<p><b>Q${i+1}</b> ${q.question}</p>`;
 
 if(q.type==="text"){
 html+=`<textarea></textarea>`;
 }
 
-html+=`</div>`;
 });
 
-document.getElementById("main").innerHTML=html;
+html+="</div>";
+
+document.getElementById("paperArea").innerHTML=html;
 }
 
-/* INIT */
 loadUnits();
