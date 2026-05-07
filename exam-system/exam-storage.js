6984b0e6d5b878948a868f745d49ddf0ae6173fd
soralatul16 <atulsoralphysicism@gmail.com>
2026-05-07 09:43:04 +0530

    Update exam-storage.js

--- exam-system/exam-storage.js
+++ exam-system/exam-storage.js
@@ -1,15 +1,24 @@
-const STORAGE_KEY = "examQuestionBank";
+const LIBRARY_KEY = "MYP_LIBRARY";
+const ATTEMPTS_KEY = "PHYS_ATTEMPTS";
 
 function getDB(){
-  return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+  return JSON.parse(localStorage.getItem(LIBRARY_KEY) || "[]");
 }
 
 function saveDB(db){
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(db));
+  localStorage.setItem(LIBRARY_KEY, JSON.stringify(db));
 }
 
 function addQuestion(q){
   let db = getDB();
   db.push(q);
   saveDB(db);
+}
+
+function getAttempts(){
+  return JSON.parse(localStorage.getItem(ATTEMPTS_KEY) || "[]");
+}
+
+function saveAttempts(arr){
+  localStorage.setItem(ATTEMPTS_KEY, JSON.stringify(arr));
 }
